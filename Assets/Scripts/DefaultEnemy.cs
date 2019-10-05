679bea571b0183368b7bb687b77f9ec325459226
clock-work <clock-work@gmx.de>
2019-10-05 15:34:07 +0200

    update

---
+++ Assets/Scripts/DefaultEnemy.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultEnemy : MonoBehaviour
+{
+    public static List<DefaultEnemy> enemies = new List<DefaultEnemy>();
+
+    private float m_moveSpeed;
+    private Vector2 m_direction;
+    private Rigidbody2D m_rigidbody;
+    private BoxCollider2D m_collider;
+
+    public static DefaultEnemy createNewEnemy(float x, float y, float width, float height, float targetX, float targetY, float moveSpeed)
+    {
+        return createNewEnemy(new Vector2(x, y), new Vector2(width, height), new Vector2(targetX, targetY), moveSpeed);
+    }
+    public static DefaultEnemy createNewEnemy(Vector2 position, Vector2 size, Vector2 direction, float moveSpeed)
+    {
+        var gameobject = Instantiate((GameObject)Resources.Load("DefaultEnemy"), new Vector3(position.x, position.y, 0), Quaternion.identity);
+        gameobject.transform.localScale = new Vector3(size.x, size.y, 1);
+        var enemy = gameobject.GetComponent<DefaultEnemy>();
+        enemy.m_direction = direction;
+        enemy.m_moveSpeed = moveSpeed;
+        return enemy;
+    }
+
+    private void Awake()
+    {
+        if (!enemies.Contains(this))
+        {
+            enemies.Add(this);
+        }
+        m_rigidbody = this.GetComponent<Rigidbody2D>();
+        m_collider = this.GetComponent<BoxCollider2D>();
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        if(enemies.Contains(this))
+        {
+            enemies.Remove(this);
+        }
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        updatePosition();
+    }
+
+
+    private void updatePosition()
+    {
+        m_rigidbody.MovePosition(m_rigidbody.position + m_direction * m_moveSpeed * Time.deltaTime);
+    }
+
+
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 1;i<6;++i)
+        {
+            DefaultEnemy.createNewEnemy(0, 0, 1, 1, i, i * 2, 1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
