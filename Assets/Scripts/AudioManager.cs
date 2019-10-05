1e09a7115fe480f512280c4235a791ef3280ee9a
0xReself <lars.m.schneider2002@gmail.com>
2019-10-05 16:44:59 +0200

    Sound | fix art

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
