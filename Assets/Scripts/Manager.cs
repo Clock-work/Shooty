49a0fc85e659b4bffacc170ebdd8ad1264d535c7
clock-work <clock-work@gmx.de>
2019-10-05 20:00:09 +0200

    bounds update

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -8,6 +8,10 @@ public class Manager : MonoBehaviour
 
     private GameObject m_leftBounds, m_rightBounds, m_topBounds, m_botBounds;
 
+    private float m_minBoundsSize;
+    private float m_maxBoundsSize;
+    private float m_currentBoundsSize;
+
     private void Awake()
     {
         instance = this;
@@ -18,11 +22,13 @@ public class Manager : MonoBehaviour
     {
         var cameraMinPos = DefaultCamera.instance.getMinPos();
         var cameraMaxPos = DefaultCamera.instance.getMaxPos();
-        int scale = 6;
-        createBounds(ref m_leftBounds, cameraMinPos.x -2, 0, scale, cameraMaxPos.y * 2);
-        createBounds(ref m_rightBounds, cameraMaxPos.x +2, 0, scale, cameraMaxPos.y * 2);
-        createBounds(ref m_topBounds, 0, cameraMinPos.y -2, cameraMaxPos.x * 2, scale);
-        createBounds(ref m_botBounds, 0, cameraMaxPos.y +2, cameraMaxPos.x * 2, scale);
+        m_minBoundsSize = 6;
+        m_maxBoundsSize = 40;
+        m_currentBoundsSize = m_minBoundsSize;
+        createBounds(ref m_leftBounds, cameraMinPos.x -2, 0, m_minBoundsSize, cameraMaxPos.y * 2);
+        createBounds(ref m_rightBounds, cameraMaxPos.x +2, 0, m_minBoundsSize, cameraMaxPos.y * 2);
+        createBounds(ref m_topBounds, 0, cameraMinPos.y -2, cameraMaxPos.x * 2, m_minBoundsSize);
+        createBounds(ref m_botBounds, 0, cameraMaxPos.y +2, cameraMaxPos.x * 2, m_minBoundsSize);
 
         for (int i = 0;i<20;++i)
         {
@@ -33,7 +39,14 @@ public class Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            changeBoundSize(1);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            changeBoundSize(-1);
+        }
     }
 
     private void createBounds(ref GameObject direction, float x, float y, float width, float height)
@@ -43,9 +56,22 @@ public class Manager : MonoBehaviour
         direction = gameobject;
     }
 
-    public void changeBoundSize()
+    //negative to decrease size for the bounds
+    public void changeBoundSize(float addedSize)
     {
-
+        m_currentBoundsSize += addedSize;
+        if(m_currentBoundsSize<m_minBoundsSize)
+        {
+            m_currentBoundsSize = m_minBoundsSize;
+        }
+        if(m_currentBoundsSize>m_maxBoundsSize)
+        {
+            m_currentBoundsSize = m_maxBoundsSize;
+        }
+        m_leftBounds.transform.localScale = new Vector3(m_currentBoundsSize, m_leftBounds.transform.localScale.y, 1);
+        m_rightBounds.transform.localScale = new Vector3(m_currentBoundsSize, m_rightBounds.transform.localScale.y, 1);
+        m_topBounds.transform.localScale = new Vector3(m_topBounds.transform.localScale.x, m_currentBoundsSize, 1);
+        m_botBounds.transform.localScale = new Vector3(m_botBounds.transform.localScale.x, m_currentBoundsSize, 1);
     }
 
 }
